827047dc03bd017e2ec8c982b06fcb42a96313df
Molnes <jakob.holkesta.molnes@gmail.com>
2023-12-09 02:51:04 +0100

    add log of start

--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
-  "fmt"
-  "net/http"
-  "time"
+	"fmt"
+	"net/http"
+	"time"
 )
 
 func greet(w http.ResponseWriter, r *http.Request) {
-  fmt.Fprintf(w, "<html><body><h1>Hello World!</h1><p>Timestamp: %s</p></body></html>", time.Now())
+	fmt.Fprintf(w, "<html><body><h1>Hello World!</h1><p>Timestamp: %s</p></body></html>", time.Now())
 }
 
 func main() {
-  http.HandleFunc("/", greet)
-  http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", greet)
+	fmt.Println("Server started, listening on port http://127.0.0.1:8080.")
+	http.ListenAndServe(":8080", nil)
 }
